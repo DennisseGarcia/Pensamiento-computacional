1383c4fefd274a61bf049184d910c99bc027f6c7
DennisseGarcia <141886246+DennisseGarcia@users.noreply.github.com>
2023-09-13 18:35:40 -0600

    Add files via upload

---
+++ Semana_6_Laboratorio_1135123_DennisseGarcía/EJ1_1135123_Sec7/EJ1_1135123_Sec7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ejercicio 1");
+        int numero; // EnvironmentVariableTarget tipo entero
+        string mes; //variable tipo cadena 
+
+        Console.WriteLine("Ingrese un numero del uno al doce:  ");
+        mes = Console.ReadLine();
+        numero = Int32.Parse(mes);
+
+        if (numero >= 1 && numero <= 12)
+        {
+            if (numero == 1)
+            {
+                Console.WriteLine("Enero");
+            }
+            else if (numero == 2)
+            {
+                Console.WriteLine("Febrero");
+            }
+            else if (numero == 3)
+            {
+                Console.WriteLine("Marzo");
+            }
+            else if (numero == 4)
+            {
+                Console.WriteLine("Abril");
+            }
+            else if (numero == 5)
+            {
+                Console.WriteLine("Mayo");
+            }
+            else if (numero == 6)
+            {
+                Console.WriteLine("Junio");
+            }
+            else if (numero == 7)
+            {
+                Console.WriteLine("Julio");
+            }
+            else if (numero == 8)
+            {
+                Console.WriteLine("Agosto");
+            }
+            else if (numero == 9)
+            {
+                Console.WriteLine("Septiembre");
+            }
+            else if (numero == 10)
+            {
+                Console.WriteLine("Octubre");
+            }
+            else if (numero == 11)
+            {
+                Console.WriteLine("Noviembre");
+            }
+            else if (numero == 12)
+            {
+                Console.WriteLine("Diembre");
+            }
+            else if (numero >= 13)
+            {
+                Console.WriteLine("Numero Invalido");
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Semana_6_Laboratorio_1135123_DennisseGarcía/L2_DEGB_1135123/L2_DEGB_1135123/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("ejercicio no 2");
+        int numero; // variable tipo entero
+        string entrada; //variable tipo cadena de texto
+
+        Console.WriteLine("Ingrese un numero del 1 al 7: ");
+        entrada = Console.ReadLine();
+        numero = Int32.Parse(entrada);
+
+        if (numero >= 1 && numero <= 7)
+        {
+            if (numero == 1)
+            {
+                Console.WriteLine("Lunes");
+            }
+            else if (numero == 2)
+            {
+                Console.WriteLine("Martes");
+            }
+            else if (numero == 3)
+            {
+                Console.WriteLine("Miercoles");
+            }
+            else if (numero == 4)
+            {
+                Console.WriteLine("Jueves");
+            }
+            else if (numero == 5)
+            {
+                Console.WriteLine("Viernes");
+            }
+            else if (numero == 6)
+            {
+                Console.WriteLine("Sabado");
+            }
+            else if (numero == 7)
+            {
+                Console.WriteLine("Domingo");
+            }
+            else if (numero >= 8)
+            {
+
+                Console.WriteLine("Numero inválido");
+      
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
+
