033a6d5c8d9252da8888e610ff3e02d3839a69f0
DennisseGarcia <141886246+DennisseGarcia@users.noreply.github.com>
2023-09-25 10:02:41 -0600

    Add files via upload

---
+++ Semana _8_Laboratorio_1135123_DennisseGarcía/L7_DEGB_1135123/L7_DEGB_1135123/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+  internal class Program
+  {
+      static void Main(string[] args)
+      {
+        //Console.WriteLine("Laboratorio semana 8");
+        //Console.WriteLine("\nIngrese un número");
+
+        //int num = Convert.ToInt32(Console.ReadLine());
+
+        //while(num != 0) 
+        //{
+        //    Console.WriteLine("El número ingresado es: " + num);
+        //    if (num > 0) 
+        //    {
+        //        Console.WriteLine("\nPositivo");
+        //    }
+        //    else 
+        //    {
+        //        Console.WriteLine("\nNegativo");
+        //    }
+        //    Console.WriteLine("\nIngrese otro número");
+
+        //    num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nIngrese un número");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int A = 0, B = 1, C = 0, i = 2;
+        string resultados = "";
+        resultados = A.ToString();
+
+        if (N > 0) 
+        {
+            resultados = A.ToString();
+        
+          if(N > 1)
+          {
+            resultados = B.ToString();
+
+            while (i < N) 
+            {
+                C = A + B;
+                resultados = C.ToString();
+                A = B;
+                B = C;
+                i++;
+            }
+                Console.WriteLine("Resultados " + resultados);
+          }
+          else 
+          {
+                Console.WriteLine("Resultados " + resultados);
+          }
+        }
+
+        else 
+        {
+            Console.WriteLine("Resultados " + resultados); 
+        }
+        Console.ReadKey();
+
+       
+
+      }
+  }
+
