a604a26738d9f3872eae69601dfe243ea464a5f6
DennisseGarcia <141886246+DennisseGarcia@users.noreply.github.com>
2023-11-06 09:13:02 -0600

    Add files via upload

---
+++ Semana_14_Laboratorio_1135123_DennisseGarcía/L14_DEGB_1135123/L14_DEGB_1135123/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+ internal class Program
+ {
+    static void Main(string[] args)
+    {
+        int[] numeros = new int[7];
+        
+        for(int i=0; i<7; i++)
+        {
+            numeros[i] = i;
+            Console.WriteLine("Ingrese un número para el índice " + i);
+            numeros[i]=int.Parse(Console.ReadLine());
+        }
+        int numMayor = numeros[0];
+        int numMenor = numeros[0];
+        for (int i =1; i<numeros.Length; i++)
+        {
+            if (numeros[i] > numMayor)
+            {
+                numMayor = numeros[i];
+            }
+            else
+            {
+                numMenor = numeros[i];
+            }
+        }
+
+        Console.WriteLine("El número mayor es: "+ numMayor);
+        Console.WriteLine("El número menor es: " + numMenor);
+        Console.ReadKey();
+
+    }
+ }
+
